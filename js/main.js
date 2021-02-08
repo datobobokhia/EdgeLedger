e7d6a713c526e7461b9cf6885354441e09f1f463
datobobokhia <dato.bobokhia@gmail.com>
2021-02-09 00:47:29 +0400

    new commit

---
+++ js/main.js
@@ -0,0 +1,42 @@
+function initMap(){
+	const loc = { lat: 41.716667, lng: 44.783333};
+	const map = new google.maps.Map(document.querySelector(".map"), 
+	{
+		zoom: 14,
+		center: loc
+	});
+
+	const marker = new google.maps.Marker({ position : loc, map: map});
+
+}
+
+// Sticky menu background //
+
+window.addEventListener('scroll', function() {
+	if (window.scrollY > 150) {
+
+		document.querySelector('#nav-bar').style.opacity = 0.9;
+
+	} else {
+		
+		document.querySelector('#nav-bar').style.opacity = 1;		
+	}
+});
+
+
+//Smooth Scrolling //
+
+$('#nav-bar a, .btn').on('click', function(event) {
+	if (this.hash !== '') {
+		event.preventDefault();
+
+		const hash = this.hash;
+
+		$('html, body').animate(
+			{
+				scrollTop: $(hash).offset().top - 100
+			},
+			800
+		);
+	}
+});	
